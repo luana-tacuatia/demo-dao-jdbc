fed30dd5093e50ff47e6ec162553d267fd2679b0
luana-tacuatia <tacuatia@yahoo.com.br>
2022-01-02 15:27:40 -0300

    feat: insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 public class Program {
@@ -21,15 +22,20 @@ public class Program {
 
         System.out.println("\n----- TEST 2: seller findByDepartment -----");
         Department department = new Department(2, null);
-        List<Seller>list = sellerDao.findByDepartment(department);
-        for (Seller obj : list){
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for (Seller obj : list) {
             System.out.println(obj);
         }
 
         System.out.println("\n----- TEST 3: seller findAll -----");
         list = sellerDao.findAll();
-        for (Seller obj : list){
+        for (Seller obj : list) {
             System.out.println(obj);
         }
+
+        System.out.println("\n----- TEST 4: seller insert -----");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
     }
 }
