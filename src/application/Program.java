cec401c8147ce25aa4d026e0c8f67298edd91475
luana-tacuatia <tacuatia@yahoo.com.br>
2022-01-02 15:02:18 -0300

    feat: findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,11 +5,11 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.List;
+
 public class Program {
 
     public static void main(String[] args) {
-
-        Department obj = new Department(1, "Books");
 
         SellerDao sellerDao = DaoFactory.createSellerDao(); //forma de injetar uma dependência sem explicitar a implementação; Assim o programa não conhece a implementação, conhece somente a interface.
 
@@ -18,5 +18,12 @@ public class Program {
         Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
+
+        System.out.println("\n----- TEST 2: seller findByDepartment -----");
+        Department department = new Department(2, null);
+        List<Seller>list = sellerDao.findByDepartment(department);
+        for (Seller obj : list){
+            System.out.println(obj);
+        }
     }
 }
