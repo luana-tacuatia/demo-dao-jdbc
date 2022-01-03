6f413e65368afd0a8ea32f3f55330e5586dfc91a
luana-tacuatia <tacuatia@yahoo.com.br>
2022-01-03 00:19:39 -0300

    feat: creating delete and update methods from DepartmentDaoJDB class

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -25,14 +25,28 @@ public class Program2 {
 
         System.out.println("\n----- TEST 2: department findAll -----");
 
-        List<Department>list = departmentDao.findAll();
-        for (Department dep : list){
+        List<Department> list = departmentDao.findAll();
+        for (Department dep : list) {
             System.out.println(dep);
         }
 
-        System.out.println("\n----- TEST 3: insert -----");
+        System.out.println("\n----- TEST 3: department insert -----");
         Department newDepartment = new Department(null, "Games");
         departmentDao.insert(newDepartment);
         System.out.println("Inserted! New id = " + newDepartment.getId());
+
+        System.out.println("\n----- TEST 4: department update -----");
+        department = departmentDao.findById(2);
+        department.setName("Kids");
+        departmentDao.update(department);
+        System.out.println("Update completed!");
+
+        System.out.println("\n----- TEST 5: department delete -----");
+        System.out.println("Enter id for delete test: ");
+        int id = sc.nextInt();
+        departmentDao.deleteById(id);
+        System.out.println("Delete completed!");
+
+        sc.close();
     }
 }
